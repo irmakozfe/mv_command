6cb42c016a0dc7affdbb1716a83189aad8c912a2
Nihal Beyza Dogan <161537635+nihalbeyzadogan@users.noreply.github.com>
2025-02-11 00:49:17 +0100

    Last update globals.c

--- src/globals.c
+++ src/globals.c
@@ -2,14 +2,13 @@
 #include <getopt.h>
 #include <stddef.h>
 
-int option_force = 0; // -f (-i ve -n etkisiz hale getirir)
-int option_interactive = 0; //-i (onay istemesi)
-int option_no_clobber = 0; // -n (dosya mevcutsa islem yapilmaz atlanir)
-int option_verbose = 0; // -v (detayli yazmasi)
+int option_force = 0; 
+int option_interactive = 0; 
+int option_no_clobber = 0; 
+int option_verbose = 0; 
 int option_help = 0;
 int option_backup = 0;
 
-// getopt_long için uzun bayraklar tanımı
 struct option long_options[] = {
     {"force", no_argument, NULL, 'f'},
     {"interactive", no_argument, NULL, 'i'},
@@ -17,6 +16,6 @@ struct option long_options[] = {
     {"verbose", no_argument, NULL, 'v'},
     {"backup", no_argument, NULL, 'b'},
     {"help", no_argument, NULL, 'h'},
-    {0, 0, 0, 0} // Sonlandırıcı
+    {0, 0, 0, 0} 
 };
 
